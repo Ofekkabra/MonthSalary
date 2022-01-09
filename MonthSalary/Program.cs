4f2d8937ebbe8d11de853e8a1a5362996011fd8e
Ofekkabra <ofek030205@gmail.com>
2022-01-09 17:19:40 +0200

    my first program

---
+++ MonthSalary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthSalary
+{
+    class Program
+    {
+        private static bool salary;
+
+        static void Main(string[] args)
+        {
+            int hours = 8;
+
+            int days = 25;
+
+            double payment = 89.4;
+
+            double total = days * hours * payment;
+
+            Console.WriteLine("salary :"+total);
+        }
+    }
+}
+
